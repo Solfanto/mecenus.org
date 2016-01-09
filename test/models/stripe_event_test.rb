e35a684e6ece269fc74344771aa37d5359ef0dd2
Matthew Nguyen <contact@n-studio.fr>
2016-01-10 00:15:59 +0700

    added test to check stripe connection

--- test/models/stripe_event_test.rb
+++ test/models/stripe_event_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class StripeEventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "stripe connects" do
+    begin
+      Stripe::Event.all(limit: 3)
+    rescue Stripe::AuthenticationError => e
+      assert false, "Stripe can't connect: #{e.message}"
+    else
+      assert true
+    end
+  end
 end
