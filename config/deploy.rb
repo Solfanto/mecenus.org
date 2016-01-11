8ebd63e3ee51cd669ffe30c3f38459671929000c
Matthew Nguyen <contact@n-studio.fr>
2016-01-12 02:49:37 +0700

    restart passenger with sudo

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,8 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
+set :passenger_restart_with_sudo, false
+
 # Sidekiq
 # set :sidekiq_queue, ['mailers', 'default']
 
