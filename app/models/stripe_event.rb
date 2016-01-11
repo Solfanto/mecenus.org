cf850d208fcb021dc2859c087da24c3cdf12bae8
Matthew Nguyen <contact@n-studio.fr>
2016-01-12 02:07:57 +0700

    fixed stripe event record

--- app/models/stripe_event.rb
+++ app/models/stripe_event.rb
@@ -2,8 +2,7 @@ class StripeEvent < ActiveRecord::Base
   include Stripe::Callbacks
 
   after_stripe_event do |target, event|
-    event_hash = event.to_hash
-    StripeEvent.create(
+    event_hash = event.as_json
       event_id: event_hash.fetch("id", nil),
       event_type: event_hash.fetch("type", nil),
       object_type: event_hash.fetch("data", {}).fetch("object", {}).fetch("object", nil), 
