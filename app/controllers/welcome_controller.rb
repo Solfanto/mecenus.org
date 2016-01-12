1fbc951446040f59e22f6ee4315ee2e4930f0a2c
Matthew Nguyen <contact@n-studio.fr>
2016-01-12 22:25:27 +0700

    fix N+1 query

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,15 @@
 class WelcomeController < ApplicationController
   def index
     if current_user
-      @projects = Project.featured.published.not_closed.last(20)
+      @projects = Project.featured.published.not_closed.includes(:user).last(20)
       if @projects.size > 0
         render :featured
       else
-        @projects = Project.published.not_closed.last(20)
+        @projects = Project.published.not_closed.includes(:user).last(20)
         render :index
       end
     else
-      @projects = Project.published.not_closed.last(20)
+      @projects = Project.published.not_closed.includes(:user).last(20)
       render :index
     end
   end
