74570e01c70b5b1a2329f33dadd30f9c0342cb02
Matthew Nguyen <contact@n-studio.fr>
2016-01-13 18:14:25 +0700

    improved search feature

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,14 @@
 class SearchController < ApplicationController
   def index
-    @projects = Project.published.not_closed.where("LOWER(title) LIKE ?", "%#{params[:q]}%")
+    @projects = []
+    
+    queries = params[:q].downcase.tokenize
+    queries.each do |query|
+      @projects += Project.published.not_closed.where("LOWER(title) LIKE ?", "%#{query}%")
+      @projects += Project.published.not_closed.where("LOWER(summary) LIKE ?", "%#{query}%")
+    end
+    @projects.uniq!
+    
+    @projects = Kaminari.paginate_array(@projects).page(params[:page]).per(20)
   end
 end
---
+++ config/initializers/tokenize.rb
@@ -0,0 +1,7 @@
+class String
+  def tokenize
+    self.split(/\s(?=(?:[^'"]|'[^']*'|"[^"]*")*$)/).
+      select {|s| not s.empty? }.
+      map {|s| s.gsub(/(^ +)|( +$)|(^["']+)|(["']+$)/,'')}
+  end
+end
