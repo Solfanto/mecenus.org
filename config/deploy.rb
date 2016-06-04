67d6542c7614518f32d1b9f5de0f4f5c6c9ed1c3
Matthew Nguyen <contact@n-studio.fr>
2016-06-04 02:35:08 +0200

    removed ruby version param from capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,6 @@ set :secret_keys, [:secret_key_base, :smtp_server, :smtp_username, :smtp_passwor
 ]
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
