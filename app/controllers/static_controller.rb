a8f73a4555f69652242592ff95f00f54257960cd
Matthew Nguyen <contact@n-studio.fr>
2016-07-03 12:47:26 +0200

    fix acme security

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def letsencrypt
     if params[:id] == ENV["LETSENCRYPT_ACME_CHALLENGE"]
-      render text: params[:id]
+      render text: params[:id] + "." + ENV["LETSENCRYPT_ACME_CHALLENGE_SECRET"]
     else
       render text: nil
     end
